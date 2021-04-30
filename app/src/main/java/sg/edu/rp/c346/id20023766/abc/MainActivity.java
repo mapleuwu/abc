b7883d60c27f97e71691c9d0194e35d5a6a7dc0f
weihong <20023766@rp.edu.sg>
2021-04-30 10:21:28 +0800

    h mh

---
+++ app/src/main/java/sg/edu/rp/c346/id20023766/abc/MainActivity.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c346.id20023766.abc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hhh
+    }
+}
